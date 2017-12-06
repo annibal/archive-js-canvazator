c4e1942fc562aab6c3f42d6f435bdbbfd766559b
Arthur Annibal Tavares-VN0IWWZ <arthur.annibal@gmail.com>
2017-12-06 12:52:59 -0200

    lol i added gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+
+var canvazatorFiles = [
+  'src/**/*.*',
+]
+
+gulp.task('clean', function() {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del(['dist']);
+});
+
+gulp.task('build:canvazator', ['clean'], function() {
+  return gulp.src(canvazatorFiles)
+    .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(concat('canvazator.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch:canvazator', function() {
+  gulp.watch(canvazatorFiles, ['build:canvazator']);
+})
+
+gulp.task('default', ['watch:canvazator']);
